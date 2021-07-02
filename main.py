6db974361f4b8010e3e863d2330fbffd75d13785
Arijit Ganguly <65554220+Arijit-datascience@users.noreply.github.com>
2021-07-02 17:29:18 +0530

    Main file changes

--- main.py
+++ main.py
@@ -69,8 +69,8 @@ def main(EPOCH, model, device, train_loader, test_loader, optimizer, scheduler, 
   for epoch in range(1, EPOCH + 1):
       print('\nEpoch {} : '.format(epoch))
       # train the model
-      train_loss, train_acc = train.train(model, device, train_loader, optimizer, scheduler, epoch, l1_factor)
-      test_loss, test_acc, test_pred, target_pred, target_data = test.test(model, device, test_loader)
+      train_loss, train_acc = train(model, device, train_loader, optimizer, scheduler, epoch, l1_factor)
+      test_loss, test_acc, test_pred, target_pred, target_data = test(model, device, test_loader)
 
       train_loss_values.append(train_loss)
       test_loss_values.append(test_loss)
