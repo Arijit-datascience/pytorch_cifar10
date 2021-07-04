07584d8e03c12f79c4a24f56faf5f532c43f4a70
Arijit Ganguly <65554220+Arijit-datascience@users.noreply.github.com>
2021-07-04 08:17:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def train(model, device, train_loader, optimizer, epoch, l1_factor):
         data, target = data.to(device), target.to(device)
         optimizer.zero_grad()
         output = model(data)
-        loss = F.nll_loss(output, target)
+        loss = F.CrossEntropyLoss()(output, target)
 
         if l1_factor > 0:
           L1_loss = nn.L1Loss(size_average=None, reduce=None, reduction='mean')
@@ -46,7 +46,7 @@ def test(model, device, test_loader):
         for data, target in test_loader:
             data, target = data.to(device), target.to(device)
             output = model(data)
-            test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
+            test_loss += F.CrossEntropyLoss()(output, target).item()  # sum up batch loss
             pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
             pred_cpu = output.cpu().data.max(dim=1, keepdim=True)[1]
             correct += pred.eq(target.view_as(pred)).sum().item()
