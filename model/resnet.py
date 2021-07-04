4ff667ce5fcd904d85214eaaae552390d2c24e66
Arijit Ganguly <65554220+Arijit-datascience@users.noreply.github.com>
2021-07-04 09:09:49 +0530

    Update resnet.py

--- model/resnet.py
+++ model/resnet.py
@@ -11,21 +11,30 @@ import torch.nn.functional as F
 class BasicBlock(nn.Module):
     expansion = 1
 
-    def __init__(self, in_planes, planes, stride=1):
+    def __init__(self, in_planes, planes, norm_type, stride=1):
         super(BasicBlock, self).__init__()
         self.conv1 = nn.Conv2d(
             in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
-        self.bn1 = nn.GroupNorm(1, planes)
+        
+        if norm_type == 'BN':
+            self.bn1 = nn.BatchNorm2d(planes)
+        else:
+            self.bn1 = nn.GroupNorm(1, planes)
+            
         self.conv2 = nn.Conv2d(planes, planes, kernel_size=3,
                                stride=1, padding=1, bias=False)
-        self.bn2 = nn.GroupNorm(1, planes)
+
+        if norm_type == 'BN':
+            self.bn2 = nn.BatchNorm2d(planes)
+        else:
+            self.bn2 = nn.GroupNorm(1, planes)
 
         self.shortcut = nn.Sequential()
         if stride != 1 or in_planes != self.expansion*planes:
             self.shortcut = nn.Sequential(
                 nn.Conv2d(in_planes, self.expansion*planes,
                           kernel_size=1, stride=stride, bias=False),
-                nn.GroupNorm(1,self.expansion*planes)
+                nn.BatchNorm2d(self.expansion*planes) if norm_type == 'BN' else nn.GroupNorm(1,self.expansion*planes)
             )
 
     def forward(self, x):
@@ -36,24 +45,28 @@ class BasicBlock(nn.Module):
         return out
 
 class ResNet(nn.Module):
-    def __init__(self, block, num_blocks, num_classes=10):
+    def __init__(self, block, num_blocks, norm_type, num_classes=10):
         super(ResNet, self).__init__()
         self.in_planes = 64
 
         self.conv1 = nn.Conv2d(3, 64, kernel_size=3,
                                stride=1, padding=1, bias=False)
-        self.bn1 = nn.GroupNorm(1, 64)
+        if norm_type == 'BN':
+           self.bn1 = nn.BatchNorm2d(64)
+        else:
+           self.bn1 = nn.GroupNorm(1,64)
+        
         self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
-        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
+        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=1)
         self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
-        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=1)
+        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
         self.linear = nn.Linear(512*block.expansion, num_classes)
 
-    def _make_layer(self, block, planes, num_blocks, stride):
+    def _make_layer(self, block, planes, num_blocks, norm_type, stride):
         strides = [stride] + [1]*(num_blocks-1)
         layers = []
         for stride in strides:
-            layers.append(block(self.in_planes, planes, stride))
+            layers.append(block(self.in_planes, planes, norm_type, stride))
             self.in_planes = planes * block.expansion
         return nn.Sequential(*layers)
 
@@ -69,9 +82,14 @@ class ResNet(nn.Module):
         return out
 
 
-def ResNet18():
-    return ResNet(BasicBlock, [2, 2, 2, 2])
+def ResNet18(norm_type='BN'):
+    if norm_type == 'BN':
+        return ResNet(BasicBlock, [2, 2, 2, 2],"BN")
+    else:
+        return ResNet(BasicBlock, [2, 2, 2, 2],"LN")
 
-
-def ResNet34():
-    return ResNet(BasicBlock, [3, 4, 6, 3])
+def ResNet34(norm_type='BN'):
+    if norm_type == 'BN':
+        return ResNet(BasicBlock, [3, 4, 6, 3],"BN")
+    else:
+        return ResNet(BasicBlock, [3, 4, 6, 3],"LN")
