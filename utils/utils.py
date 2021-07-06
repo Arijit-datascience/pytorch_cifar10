d561c6bb0d65a53cc62bfdc3f918db3a7fe523a4
Rohin Sequeira <31658286+RohinSequeira@users.noreply.github.com>
2021-07-06 23:06:12 +0530

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -58,7 +58,6 @@ def get_transforms(norm_mean,norm_std):
                                   A.Rotate(limit=5),
                                   A.Normalize(mean=norm_mean, std=norm_std,always_apply=True),
                                   ToTensorV2()
-                               ]
                                )
 
     test_transform = A.Compose([A.Normalize(norm_mean, norm_std, always_apply=True), ToTensorV2()])
