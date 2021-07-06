369ab051bd9ec2b11f99ca15999e5466de18c24b
Arijit Ganguly <arijitganguly35@gmail.com>
2021-07-06 22:06:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,14 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 
+EPOCH = 40
+batch_size = 256
+l1_factor = 0
+l2_factor = 0.0001
+lr_value = 0.001
+mt_val = 0.9
+
+
 def train(model, device, train_loader, optimizer, epoch, l1_factor):
     model.train()
     epoch_loss = 0
