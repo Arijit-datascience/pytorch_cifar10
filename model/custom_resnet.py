1464925f2f922d10bd82324fcd5047a24adfa587
Rohin Sequeira <31658286+RohinSequeira@users.noreply.github.com>
2021-07-09 15:10:55 +0530

    Update custom_resnet.py

--- model/custom_resnet.py
+++ model/custom_resnet.py
@@ -38,14 +38,64 @@ class ResNet(nn.Module):
     def __init__(self, block, num_blocks, num_classes=10):
         super(ResNet, self).__init__()
         self.in_planes = 64
-
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=3,
-                               stride=1, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(64)
-        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
-        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=1)
-        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
-        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
+        
+        #Prep Layer
+        self.prep = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=64, kernel_size=3, stride=1, padding=1, bias=False)
+            nn.BatchNorm2d(64)
+            nn.ReLU()
+        )
+        
+        #Layer1
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=(3, 3), stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(128),
+            nn.ReLU()   
+        )
+        
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(3, 3), padding=1, bias=False),
+            nn.BatchNorm2d(128),
+            nn.ReLU()
+        )
+        
+        self.conv4 = nn.sequential(
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(3, 3), padding=1, bias=False),
+            nn.BatchNorm2d(128),
+            nn.ReLU()
+        )
+        
+        #Layer2
+        self.conv5 = nn.Sequential(
+            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=(3, 3), padding=1, bias=False),
+            nn.MaxPool2d(2, 2) 
+            nn.BatchNorm2d(256),
+            nn.ReLU()
+        )
+        
+        #Layer3
+        self.conv6 = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=32, kernel_size=(3, 3), padding=1, bias=False),
+            nn.MaxPool2d(2, 2) 
+            nn.BatchNorm2d(32),
+            nn.ReLU()
+        )
+        
+        self.conv6 = nn.Sequential(
+            nn.Conv2d(in_channels=256, out_channels=512, kernel_size=(3, 3), padding=1, bias=False),
+            nn.BatchNorm2d(512),
+            nn.ReLU()
+        )
+        
+        self.conv7 = nn.Sequential(
+            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=(3, 3), padding=1, bias=False),
+            nn.BatchNorm2d(512),
+            nn.ReLU()
+        )
+        
+        self.max = nn.Sequential(
+            nn.MaxPool2d(kernel_size=4) )
+        
         self.linear = nn.Linear(512*block.expansion, num_classes)
 
     def _make_layer(self, block, planes, num_blocks, stride):
