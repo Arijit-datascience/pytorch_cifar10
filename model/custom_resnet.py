87a0f134bab0cc4ef8ab327834947d660eea4aee
Rohin Sequeira <31658286+RohinSequeira@users.noreply.github.com>
2021-07-09 16:23:06 +0530

    Update custom_resnet.py

--- model/custom_resnet.py
+++ model/custom_resnet.py
@@ -2,38 +2,6 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-
-
-class BasicBlock(nn.Module):
-    expansion = 1
-
-    def __init__(self, in_planes, planes, stride=1):
-        super(BasicBlock, self).__init__()
-        self.conv1 = nn.Conv2d(
-            in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(planes)
-        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3,
-                               stride=1, padding=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(planes)
-        self.shortcut = nn.Sequential()
-        if stride != 1 or in_planes != self.expansion*planes:
-            self.shortcut = nn.Sequential(
-                nn.Conv2d(in_planes, self.expansion*planes,
-                          kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(self.expansion*planes)
-            )
-
-    def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
-        out = self.bn2(self.conv2(out))
-        out += self.shortcut(x)
-        out = F.relu(out)
-        return out
-
 class ResNet(nn.Module):
     def __init__(self, num_classes=10):
         super(ResNet, self).__init__()
@@ -128,10 +96,3 @@ class ResNet(nn.Module):
         out = out.view(out.size(0), -1)
         out = self.linear(out)
         return F.log_softmax(out, dim=-1)
-
-
-def ResNet18():
-   return ResNet(BasicBlock, [2, 2, 2, 2])
-
-def ResNet34():
-    return ResNet(BasicBlock, [3, 4, 6, 3])
