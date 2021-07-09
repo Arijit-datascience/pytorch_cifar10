d896035526b1f7fdc47696f36ca99b4cd1f5489d
Rohin Sequeira <31658286+RohinSequeira@users.noreply.github.com>
2021-07-09 15:53:34 +0530

    Update custom_resnet.py

--- model/custom_resnet.py
+++ model/custom_resnet.py
@@ -35,7 +35,7 @@ class BasicBlock(nn.Module):
         return out
 
 class ResNet(nn.Module):
-    def __init__(self, block, num_blocks, num_classes=10):
+    def __init__(self, num_classes=10):
         super(ResNet, self).__init__()
         self.in_planes = 64
         
