22940bf3b2f140c7a17ac5655d49453a1409e51a
Rohin Sequeira <31658286+RohinSequeira@users.noreply.github.com>
2021-07-16 19:00:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import torch.nn.functional as F
 from torch.autograd import Variable
 
 def train(model, device, train_loader, optimizer, epoch, l1_factor, scheduler):
+    #Function for training model
     model.train()
     epoch_loss = 0
     correct = 0
@@ -37,63 +38,8 @@ def train(model, device, train_loader, optimizer, epoch, l1_factor, scheduler):
     train_acc=100.*correct/len(train_loader.dataset)
     return train_loss, train_acc
 
-def train_old(model, device, train_loader, optimizer, epoch, l1_factor):
-    model.train()
-    epoch_loss = 0
-    correct = 0
-
-    for batch_idx, (data, target) in enumerate(train_loader):
-        data, target = data.to(device), target.to(device)
-        optimizer.zero_grad()
-        output = model(data)
-        loss = nn.CrossEntropyLoss()(output, target)
-        
-        reg_loss = 0 
-        if l1_factor > 0:
-            for p in model.parameter():
-                reg_loss = reg_loss + p.abs().sum()
-
-        loss += l1_factor * reg_loss
-
-        epoch_loss += loss.item()
-        loss.backward()
-        optimizer.step()
-        pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
-        correct += pred.eq(target.view_as(pred)).sum().item()
-
-    print(f'Train set: Average loss: {loss.item():.4f}, Accuracy: {100. * correct/len(train_loader.dataset):.2f}')
-    train_loss = epoch_loss / len(train_loader)
-    train_acc=100.*correct/len(train_loader.dataset)
-    return train_loss, train_acc
-
-def cycle_lr(model,train_loader,clr,optimizer):
-    running_loss = 0.
-    avg_beta = 0.98
-    model.train()
-    for i, (input, target) in enumerate(train_loader):
-        input, target = input.cuda(), target.cuda() 
-        #var_ip, var_tg = Variable(input), Variable(target)
-        output = model(input)
-        #loss = F.nll_loss(output, var_tg)
-        loss = nn.CrossEntropyLoss()(output, target)
-    
-        running_loss = avg_beta * running_loss + (1-avg_beta) *loss.item()
-        smoothed_loss = running_loss / (1 - avg_beta**(i+1))
-    
-        lr = clr.calc_lr(smoothed_loss)
-        if lr == -1 :
-            break
-        for g in optimizer.param_groups:
-            g['lr'] = lr
-    
-        # compute gradient and do SGD step
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-    
-    return clr
-
-def test(model, device, test_loader):    
+def test(model, device, test_loader):
+    #Function for training model
     model.eval()
     test_loss = 0
     correct = 0
@@ -113,6 +59,7 @@ def test(model, device, test_loader):
     return test_loss, test_acc
 
 def main(EPOCH, model, device, train_loader, test_loader, optimizer, scheduler, l1_factor):
+  #This function controls the run for train/test of model for number of epochs as passed in parameters
   train_loss_values = []
   test_loss_values = []
   train_acc_values = []
